1774204179cf92e4fc50636c375099093c921d11
teszerrakt <zaky.syihab@gmail.com>
2022-11-30 12:44:04 +0700

    fix: header styling

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,13 @@
 import styles from './styles.module.scss'
+import logo from '@/assets/react.svg'
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <div className={styles.logoContainer}>LAZY INTERVIEW</div>
+      <div className={styles.logoContainer}>
+        <img src={logo} alt="logo" />
+        LAZY INTV
+      </div>
       <div className={styles.navContainer}>
         <a>Home</a>
         <a>Favorites</a>
