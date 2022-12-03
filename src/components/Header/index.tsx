e85608eccd61ebf9376e19ff7740e9a3677955db
teszerrakt <zaky.syihab@gmail.com>
2022-12-03 16:16:05 +0700

    feat: add disappearing header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,10 +2,19 @@ import styles from './styles.module.scss'
 import logo from '@/assets/react.svg'
 import { NavButtons } from '../Navigation'
 import { NAV_ITEMS } from '../Navigation/config'
+import { useScrollDirection } from '../../hooks/useScrollDirection'
 
 const Header = () => {
+  const scrollDirection = useScrollDirection()
+  const isScrollDown = scrollDirection === 'down'
+
   return (
-    <header className={styles.header}>
+    <header
+      className={styles.header}
+      style={{
+        top: isScrollDown ? -100 : 0,
+      }}
+    >
       <div className={styles.logoContainer}>
         <img src={logo} alt="logo" />
         LAZY INTV
